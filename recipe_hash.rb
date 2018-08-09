a42cd7a391e1b8feb282bc4ca934016f44d244f3
jevina <jwong00@stuy.edu>
2018-08-09 15:09:08 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -26,13 +26,13 @@ perfect_10_recipe = {:almond_flour => "2 cups", :gluten_freewhole_oats => "1 cup
 
 # puts perfect_10_recipe
 
-#2. Write a method that simply returns (not puts) your perfect_10 hash
+#1. Write a method that simply returns (not puts) your perfect_10 hash
 
 def recipe_ingredients(perfect_10_recipe)
   return perfect_10_recipe
 end
 
-puts recipe_ingredients(perfect_10_recipe)
+# puts recipe_ingredients(perfect_10_recipe)
 
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
 
